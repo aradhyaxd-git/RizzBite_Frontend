83c7aa66a5de790c0be52fccd757034f3a70f428
Aradhya <2k23.cs2312107@gmail.com>
2025-08-24 21:19:58 +0530

    fix:logo

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import logo from '../../assets/icons/logo.svg';
+import logo from '../../assets/logo.svg';
 import { Github, LinkedinIcon } from 'lucide-react';
 
 const Footer = () => {
